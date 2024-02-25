1952ac511095fc6db12cc95abf398dd6d1341775
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-02-25 03:54:50 +0100

    component displays help

---
+++ src/responses/Help.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Help() {
+    const commands = [
+        ['help', 'displays this'],
+        ['theme/themes', 'shows options for chganing theme'],
+        ['kitten', 'generates kitten picture :3']
+    ]
+
+    return (
+        <div className="grid grid-cols-2 w-fit">
+            {commands.map((command) => <><span className="text-splash tab">&nbsp;&nbsp;'{command[0]}'</span><span>{command[1]}</span></>)}
+        </div>
+    )
+}
