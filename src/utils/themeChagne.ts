969cd16004699532b851dc9c5aab49ef8881996d
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-02-25 03:52:47 +0100

    added get theme function

--- src/utils/themeChagne.ts
+++ src/utils/themeChagne.ts
@@ -1,3 +1,7 @@
 export const changeTheme = (theme: string) => {
     document.querySelector("html")?.setAttribute("data-theme", theme);
 };
+
+export const getTheme = () => {
+    return document.querySelector("html")?.getAttribute("data-theme");
+};
