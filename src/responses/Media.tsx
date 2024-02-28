17769b578c3f2ad07b7319352481bf2c1293af4a
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-02-28 23:24:12 +0100

    media command showin social media

--- src/responses/Media.tsx
+++ src/responses/Media.tsx
@@ -1,6 +1,31 @@
-
+import { FaDiscord, FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
 export default function Media() {
     return (
-        <div></div>
+        <div className="grid grid-cols-2 w-fit gap-y-2 [&>*]:flex [&>*]:flex-row [&>*]:gap-3">
+            <div>
+            &nbsp;
+            <FaGithub className="scale-110 mt-[2px]"/>
+                <p>Github</p>
+            </div>
+            <a className="text-splash underline" href="https://www.github.com/jeffizmyname" target="_blank">github.com/jeffizmyname</a>
+            <div>
+            &nbsp;
+            <FaFacebook className="scale-110 mt-[2px]"/>
+                <a>Facebook</a>
+            </div>
+            <a className="text-splash underline" href="https://www.facebook.com/jeffizmyname/" target="_blank">facebook.com/jeffizmyname</a>
+            <div>
+            &nbsp;
+            <FaInstagram className="scale-110 mt-[2px]"/>
+                <a>Instagram</a>
+            </div>
+            <a className="text-splash underline" href="https://www.instagram.com/stanislaws_solarz" target="_blank">instagram.com/stanislaws_solarz</a>
+            <div>
+            &nbsp;
+            <FaDiscord className="scale-110 mt-[2px]"/>
+                <a>Discord</a>
+            </div>
+            <a className="text-splash underline" href="https://www.discordapp.com/users/376306346980802563" target="_blank">discordapp.com/users/376306346980802563</a>
+        </div>
     )
 }
